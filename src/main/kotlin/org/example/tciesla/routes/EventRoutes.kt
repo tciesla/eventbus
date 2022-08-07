2eb7e23d1d4f22b2ad9be94ed7eaffa7114f58ba
Tomasz Cieśla <ciesla.tomasz.92@gmail.com>
2022-08-07 12:58:27 +0200

    event post endpoint

--- src/main/kotlin/org/example/tciesla/routes/EventRoutes.kt
+++ src/main/kotlin/org/example/tciesla/routes/EventRoutes.kt
@@ -1,6 +1,8 @@
 package org.example.tciesla.routes
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.example.tciesla.models.Event
@@ -8,14 +10,21 @@ import java.time.OffsetDateTime
 import java.util.*
 
 fun Route.eventRouting() {
+
+    val eventsInMemory = mutableListOf<Event>(
+        Event(UUID.randomUUID(), OffsetDateTime.now(), "somePayload1"),
+        Event(UUID.randomUUID(), OffsetDateTime.now(), "somePayload2"),
+        Event(UUID.randomUUID(), OffsetDateTime.now(), "somePayload3")
+    )
+
     route("/") {
         get {
-            val exampleEvents = listOf(
-                Event(UUID.randomUUID(), OffsetDateTime.now(), "somePayload1"),
-                Event(UUID.randomUUID(), OffsetDateTime.now(), "somePayload2"),
-                Event(UUID.randomUUID(), OffsetDateTime.now(), "somePayload3")
-            )
-            call.respond(exampleEvents)
+            call.respond(eventsInMemory)
+        }
+        post {
+            val event = call.receive<Event>()
+            eventsInMemory.add(event)
+            call.respond(HttpStatusCode.Created)
         }
     }
 }
